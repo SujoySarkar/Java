d05f7d9d8bd9d11760385d8be4856c4b9e804abf
SujoySarkar <sujoy2838@gmail.com>
2020-02-22 11:32:53 +0600

    java

---
+++ Object Oriented Programmimg/Inheritance/Hierarchical Inheritance/mainMethod.java
@@ -0,0 +1,11 @@
+/**
+ * mainMethod
+ */
+public class mainMethod {
+    public static void main(String args[]){
+        Cat c=new Cat();
+        c.meow();
+        c.eat();
+        }
+        
+}
